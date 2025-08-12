82c621eb0e532c610a9483d235c28e57da01c074
Letícia Cerial <leticia.cerial@aluno.senai.br>
2025-08-12 12:21:46 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,68 @@
 const express = require('express');
 const app = express();
 
-//permitir receber dadosem JSON
+// Permite receber dados em JSON
 app.use(express.json());
 
-//simula em "banco de dados" em memoria
-let produtos =[
-    {id: 1, nome:"Mouse"},
-    {id: 2, nome:"Teclado"}
+// Simula um "banco de dados" na memória
+let produtos = [
+    { id: 1, nome: "Mousse" },
+    { id: 2, nome: "Teclado" },
 ];
 
-//GET = Lista todos os produtos
-app.get('/api/produtos', (req,res) =>{
-    res.json (produtos);
-})
+// GET = Lista todos os produtos
+app.get('/api/produtos', (req, res) => {
+    res.json(produtos);
+});
 
-//post
-app.post('/api/produtos', (req,res) =>{
+// POST - Adiciona um novo produto
+app.post('/api/produtos', (req, res) => {
     const novoProduto = {
-        id:produto.lenght +1,
-        nome:req.body.nome
+        id: produtos.length + 1,
+        nome: req.body.nome
     };
     produtos.push(novoProduto);
     res.status(201).json(novoProduto);
-})
-
-//rota principal
-app.get('/', (req, res) =>{
-    res.send('Olá, este é o servidor com Express');
-})
-
-//rota "sobre"
-app.get('/sobre',(req,res)=>{
-    res.send('pagina sobre');
 });
 
-//rota "produtos"
-app.get('/produtos',(req,res)=>{
+// PUT - Atualiza um produto
+app.put('/api/produtos/:id', (req, res) => {
+    const id = parseInt(req.params.id); 
+    const produto = produtos.find(p => p.id === id); 
+
+    if (!produto) {
+        return res.status(404).json({ mensagem: 'Produto não encontrado!' }); 
+    }
+
+    produto.nome = req.body.nome;
+    res.json(produto);
+});
+
+// DELETE - Remove um produto
+app.delete('/api/produtos/:id',(req,res)=>{
+    const  id = parseInt(req,params.id);
+  if(isNaN(id)){
+    return res.status(400).json({mensagem: 'O ID fornecido não é valido '})
+  }
+  const tamanhoOriginal = produtos.length;
+  produtos = produtos.filter(p => p.id !== id );
+  if (tamanhoOriginal === produtos.length){
+    return res.status(404).json({mensagem:'Produto não encontrado para exclussão'})
+  }
+});
+
+// Rota principal
+app.get('/', (req, res) => {
+    res.send('Olá, este é o servidor com Express');
+});
+
+// Rota "sobre"
+app.get('/sobre', (req, res) => {
+    res.send('Página Sobre');
+});
+
+// Rota "produtos"
+app.get('/produtos', (req, res) => {
     res.send('Lista de Produtos');
 });
 
@@ -50,7 +76,7 @@ app.get('/api/produtos', (req,res)=>{
 
 })
 
-//inicia o servidor na portal 3000
-app.listen(3000, () =>{
-    console.log('Servidor rodando http://localhost:3000');
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
 });
